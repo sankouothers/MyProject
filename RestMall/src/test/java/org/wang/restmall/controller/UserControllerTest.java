618aa9635a70906ffe4652cf474147ea25481168
Pin Wang <pin.wang@ozstrategy.com>
2018-03-06 10:56:41 +0800

    add userController test

--- RestMall/src/test/java/org/wang/restmall/controller/UserControllerTest.java
+++ RestMall/src/test/java/org/wang/restmall/controller/UserControllerTest.java
@@ -6,9 +6,16 @@ import static org.hamcrest.core.Is.is;
 
 import org.junit.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.MediaType;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.wang.restmall.service.UserService;
 
 
 /**
@@ -18,8 +25,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @version  $Revision$, $Date$
  */
 public class UserControllerTest extends BaseControllerTest {
+  //~ Instance fields --------------------------------------------------------------------------------------------------
+
+  /** DOCUMENT ME! */
+  @Autowired UserService userService;
+
   //~ Methods ----------------------------------------------------------------------------------------------------------
 
+  /**
+   * DOCUMENT ME!
+   *
+   * @throws  Exception  DOCUMENT ME!
+   */
+  @Test public void createUser() throws Exception {
+    //J-
+    mockMvc.perform(post("/user")
+        .param("name","li si")
+        .param("passWord","wang123")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+        .andExpect(status().isOk());
+    //J+
+  }
+
+  //~ ------------------------------------------------------------------------------------------------------------------
 
   /**
    * DOCUMENT ME!
@@ -35,4 +63,21 @@ public class UserControllerTest extends BaseControllerTest {
     //J+
   }
 
-}
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * DOCUMENT ME!
+   *
+   * @throws  Exception  DOCUMENT ME!
+   */
+  @Test public void userList() throws Exception {
+    //J-
+    mockMvc.perform(get("/user"))
+        .andExpect(jsonPath("$[2].id", is(equalTo(3))))
+        .andExpect(jsonPath("$[2].name", is(equalTo("san kou"))))
+        .andExpect(jsonPath("$[2].passWord", is(equalTo("P123456"))))
+        .andExpect(status().isOk());
+    //J+
+  }
+
+} // end class UserControllerTest
