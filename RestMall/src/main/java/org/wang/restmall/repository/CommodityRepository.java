7d02785d9ad7e03e2048f43e6cb7e2d0aa93e495
Pin Wang <pin.wang@ozstrategy.com>
2018-03-09 14:28:43 +0800

    添加新的 service

---
+++ RestMall/src/main/java/org/wang/restmall/repository/CommodityRepository.java
@@ -0,0 +1,13 @@
+package org.wang.restmall.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wang.restmall.model.Role;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   <a href="mailto:pin.wang@ozstrategy.com">Pin Wang</a>
+ * @version  $Revision$, $Date$
+ */
+public interface CommodityRepository extends CrudRepository<Role, Long> { }
---
+++ RestMall/src/main/java/org/wang/restmall/service/impl/RoleServiceImpl.java
@@ -0,0 +1,16 @@
+package org.wang.restmall.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import org.wang.restmall.service.RoleService;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   <a href="mailto:pin.wang@ozstrategy.com">Pin Wang</a>
+ * @version  $Revision$, $Date$
+ */
+@Service @Transactional public class RoleServiceImpl implements RoleService { }
